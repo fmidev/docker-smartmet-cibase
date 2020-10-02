ede18170b7b8866ea0862ec9c0ea342feedcba99
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-10-02 08:45:37 +0300

    Detect RHEL7 version when choosing image name file

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -ex
 
 cd `dirname $0`
+
+# Establish OS version
+if grep -q -i "release 7" /etc/redhat-release ; then
+  imagenamefile=imagename7.txt
+elif grep -q -i "release 8" /etc/redhat-release ; then
+  imagenamefile=imagename8.txt
+else
+  echo "Running neither RHEL7.x nor RHEL 8.x !"
+  exit 1
+fi
+
+
 testfile=.buildage
 
 # Force rebuild without cache every 24 hours
@@ -13,9 +25,9 @@ test ! -r "$testfile" || (
 param=""
 
 if [ -e "$testfile" ] ; then
-	docker build -t `cat imagename.txt` .
+	docker build -t $(cat $imagenamefile) .
 else
-	docker build --no-cache -t `cat imagename.txt` .
+	docker build --no-cache -t $(cat $imagenamefile) .
 fi
 
 # Create timestamp with current time if not already there
