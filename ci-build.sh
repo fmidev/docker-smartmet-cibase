fe10a6b0fefd131133abb2b3d0e80d991a7ed55b
Mika Heiskanen <mika.heiskanen@fmi.fi>
2021-01-11 12:47:35 +0200

    Use g++ instead of clang++ to see if WMS tests start working

--- ci-build.sh
+++ ci-build.sh
@@ -127,11 +127,11 @@ for step in $* ; do
 	    test -r $test_disable && (
 	       set +x 
 	       echo "Test step disabled by existence of $test_disable, remove to enable tests"
-	       cat $test_disable  ) || CXX=clang++ make -j "$RPM_BUILD_NCPUS" test
+	       cat $test_disable  ) || CXX=g++ make -j "$RPM_BUILD_NCPUS" test
 	    ;;
 	rpm)
             set +x
-	    CXX=clang++ make -j "$RPM_BUILD_NCPUS" rpm
+	    CXX=g++ make -j "$RPM_BUILD_NCPUS" rpm
 	    tmpd=`mktemp -d`
 	    for d in /root/rpmbuild $HOME/rpmbuild ; do
 			test ! -d "$d" || find "$d" -name \*.rpm -exec sudo mv -v {} "$tmpd" \;
